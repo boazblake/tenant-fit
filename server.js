4f366ada12fa0371ca4e1773ebf9c6bacf1dc919
boaz blake <boazblake@gmail.com>
2017-09-08 22:52:36 -0500

    adding server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var port = Number(process.env.PORT || 9000);
 
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + 'dist/index'));
 
 var server = app.listen(port, function() {
    console.log('Listening on port %d', server.address().port);
