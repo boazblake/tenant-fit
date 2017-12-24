4a5571a25bd4733f6327817d13d10597789a2094
Boaz Blake <boazblake@gmail.com>
2017-12-24 10:56:20 -0600

    removing path

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 9000;
-app.listen(port);
-console.log('server started '+ port);
+var express = require('express')
+var serveStatic = require('serve-static')
+app = express()
+app.use(serveStatic(__dirname + '/dist'))
+var port = process.env.PORT || 9000
+app.listen(port)
+console.log('server started ' + port)
